f32ccda7684fec621e53d60079c7eddf9c9c5625
DongJinBae <qaz964@gmail.com>
2022-06-25 02:57:12 +0900

    changed managers singleton
    changed managers script singleton pattern

--- UnityPRJ/Assets/Scripts/Managers/Managers.cs
+++ UnityPRJ/Assets/Scripts/Managers/Managers.cs
@@ -4,10 +4,36 @@ using UnityEngine;
 
 public class Managers : MonoBehaviour
 {
+    static Managers s_instance;
+    public static Managers Instance
+    {
+        get
+        {
+            Init();
+            return s_instance;
+        }
+    }
+
+    static void Init()
+    {
+        if(s_instance == null)
+        {
+            GameObject gobj = GameObject.Find("@Managers");
+            if(gobj == null)
+            {
+                gobj = new GameObject { name = "@Managers" };
+                gobj.AddComponent<Managers>();
+            }
+
+            DontDestroyOnLoad(gobj);
+            s_instance = gobj.GetComponent<Managers>();
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Init();
     }
 
     // Update is called once per frame
@@ -15,4 +41,5 @@ public class Managers : MonoBehaviour
     {
         
     }
+
 }
